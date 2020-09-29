f5a97c5aa3e90f99dd98bbc4697ed9bcad13a160
dixitvora <dixitvora@gmail.com>
2020-09-29 06:09:14 +0530

    The initial stage and set up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hell From Dax Code </h1>
+    </div>
+  );
+}
+
+export default App;
