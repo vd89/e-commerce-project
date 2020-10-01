94b08c1b91e366894638b1a3fc29d283bf47748b
dixitvora <dixitvora@gmail.com>
2020-10-01 11:53:57 +0530

    Chore: Add local storage with react persist

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './rootReducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
+export const persistor = persistStore(store);
 
-export default store;
+export default { store, persistor };
