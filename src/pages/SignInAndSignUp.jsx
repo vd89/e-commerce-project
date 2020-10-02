9c061ccb01c2fff2b6291e49408ab6a881d54ff8
dixitvora <dixitvora@gmail.com>
2020-10-02 17:54:03 +0530

    Fix: signin and signup form

--- src/pages/SignInAndSignUp.jsx
+++ src/pages/SignInAndSignUp.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import SignIn from '../component/signInAndsignUp/signIn/SignIn';
 import SignUp from '../component/signInAndsignUp/signUp/SignUp';
+import styled from 'styled-components';
 
+const SignInAndSignUpContainer = styled.div`
+  width: 850px;
+  display: flex;
+  justify-content: space-between;
+  margin: 30px auto;
+`;
 const SignInAndSignUp = () => {
   return (
-    <div className='signIn__singUp'>
+    <SignInAndSignUpContainer>
       <SignIn />
       <SignUp />
-    </div>
+    </SignInAndSignUpContainer>
   );
 };
 
